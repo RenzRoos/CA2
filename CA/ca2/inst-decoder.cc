99747e98782c7d46a4bef1b3322ed31352ea77a4
RenzRoos <90395531+RenzRoos@users.noreply.github.com>
2021-11-08 14:00:47 +0100

    Add files via upload

---
+++ CA/ca2/inst-decoder.cc
@@ -0,0 +1,54 @@
+/* rv64-emu -- Simple 64-bit RISC-V simulator
+ *
+ *    inst-decoder.cc - RISC-V instruction decoder.
+ *
+ * Copyright (C) 2016,2019  Leiden University, The Netherlands.
+ *
+ */
+
+#include "inst-decoder.h"
+
+#include <functional>
+#include <map>
+
+
+/*
+ * Class InstructionDecoder -- helper class for getting specific
+ * information from the decoded instruction.
+ */
+
+void
+InstructionDecoder::setInstructionWord(const uint32_t instructionWord)
+{
+  this->instructionWord = instructionWord;
+}
+
+uint32_t
+InstructionDecoder::getInstructionWord() const
+{
+  return instructionWord;
+}
+
+RegNumber
+InstructionDecoder::getRS1() const
+{
+  /* TODO: implement */
+
+  return 0;  /* result undefined */
+}
+
+RegNumber
+InstructionDecoder::getRS2() const
+{
+  /* TODO: implement */
+
+  return 0;  /* result undefined */
+}
+
+RegNumber
+InstructionDecoder::getRD() const
+{
+  /* TODO: implement */
+
+  return 0; /* result undefined */
+}
---
+++ CA/ca2/inst-formatter.cc
@@ -0,0 +1,23 @@
+/* rv64-emu -- Simple 64-bit RISC-V simulator
+ *
+ *    inst-formatter.cc - RISC-V instruction printer (disassembler)
+ *
+ * Copyright (C) 2016,2018  Leiden University, The Netherlands.
+ */
+
+#include "inst-decoder.h"
+
+#include <functional>
+#include <map>
+#include <iostream>
+
+
+std::ostream &
+operator<<(std::ostream &os, const InstructionDecoder &decoder)
+{
+  /* TODO: write a textual representation of the decoded instruction
+   * in "decoder" to the output stream "os".
+   */
+
+  return os;
+}
